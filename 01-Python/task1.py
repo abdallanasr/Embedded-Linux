39cf9b79d66327bad8200d261d869cb190599101
abdalla <abdalla@Abdalla.Nasr>
2024-06-14 14:23:07 +0300

    task 3 for python

---
+++ 01-Python/task1.py
@@ -0,0 +1,2 @@
+#Print all info in one line
+print("Full Name : Abdalla Nasr Farag \nBirth : 28/4/2002 \nFaculty : SCU \nE-mail : abdallanasr280@gmail.com \nAddress : Ismailia ")
---
+++ 01-Python/task2.py
@@ -0,0 +1,7 @@
+x = 10
+y = 15
+
+print("X + y = " + (str)(x + y))
+print("X - y = " + (str)(x - y))
+print("X / y = " + (str)(x / y))
+print("X * y = " + (str)(x * y))
