639ffcf916cddd6c3e2c092f8a05ec771e0bcfdd
Abdalla Nasr <abdallanasr280@gmail.com>
2024-06-26 03:27:30 +0300

    Last update

--- 01-Python/Lec_2/task2.py
+++ 01-Python/Lec_2/task2.py
@@ -1,9 +1,10 @@
 import requests
 import webbrowser
 
-response = requests.get("https://api.ipify.org/?format=json")
+ip_response = requests.get("https://api.ipify.org/?format=json")
+ip_address = ip_response.json()["ip"]
 
-ip_address = response.json()['ip']
+location_response = requests.get(f"https://ipinfo.io/{ip_address}/geo")
+location_data = location_response.json()
 
-webbrowser.open(f"https://ipinfo.io/{ip_address}/geo")
-
+print(f"I'm currently in {location_data['city']}, {location_data['country']}.")
