fcd78acbbc49130c852e5cfe35362ae2d506d68e
Abdalla Nasr <abdallanasr280@gmail.com>
2024-06-27 02:50:07 +0300

    Last update

---
+++ 01-Python/Test/module.py
@@ -0,0 +1,11 @@
+def sum(x = 0, y = 0):
+    print(f"x + y = {x+y}")
+
+def sub(x = 0, y = 0):
+    print(f"x - y = {x-y}")
+
+def multi(x = 0, y = 0):
+    print(f"x * y = {x*y}")
+
+def div(x = 0, y = 0):
+    print(f"x / y = {x/y}")
